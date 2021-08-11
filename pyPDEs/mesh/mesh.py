fa394c3c630da03c9495e0d5e3675baa8bcb4b12
Zachary Hardy <zhardy1115@gmail.com>
2021-08-11 11:44:13 -0600

    Updated documentation, added n_faces.

--- pyPDEs/mesh/mesh.py
+++ pyPDEs/mesh/mesh.py
@@ -20,49 +20,134 @@ class Mesh:
         self.vertices: List[Vector] = []
         self.boundary_cell_ids: List[int] = []
 
+        self._n_faces: int = 0
+
     @property
     def n_cells(self) -> int:
+        """
+        Get the total number of cells in the mesh.
+
+        Returns
+        -------
+        int
+        """
         return len(self.cells)
 
     @property
     def n_faces(self) -> int:
-        if self.dim == 1:
-            return self.n_cells + 1
-        else:
-            raise NotImplementedError(
-                f"Only 1D meshes have been implemented.")
+        """
+        Get the total number of faces in the mesh. This is
+        a bit tricky because there is no defined list of
+        unique faces. To compute the total number of faces,
+        we look over cells and faces, counting each interior
+        face as half and each boundary face as one. This way,
+        when all perspectives of an interior face are encountered,
+        the face accounts for one unique face.
+
+        Returns
+        -------
+        int
+        """
+        if self._n_faces == 0:
+            for cell in self.mesh.cells:
+                for face in cell.faces:
+                    if face.has_neighbor:
+                        self._n_faces += 0.5
+                    else:
+                        self._n_faces += 1.0
+            self._n_faces = int(self._n_faces)
+        return self._n_faces
 
     @property
     def n_vertices(self) -> int:
+        """
+        Get the total number of vertices on the mesh.
+
+        Returns
+        -------
+        int
+        """
         return len(self.vertices)
 
     @property
     def x_min(self) -> float:
+        """
+        Get the minimum x-coordinate over all vertices.
+        This is a marker for the left boundary.
+
+        Returns
+        -------
+        float
+        """
         return np.min([v.x for v in self.vertices])
 
     @property
     def x_max(self) -> float:
+        """
+        Get the maximum x-coordinate over all vertices.
+        This is a marker for the right boundary.
+
+        Returns
+        -------
+        float
+        """
         return np.max([v.x for v in self.vertices])
 
     @property
     def y_min(self) -> float:
+        """
+        Get the minimum y-coordinate over all vertices.
+        This is a marker for the front boundary.
+
+        Returns
+        -------
+        float
+        """
         return np.min([v.y for v in self.vertices])
 
     @property
     def y_max(self) -> float:
+        """
+        Get the maximum y-coordinate over all vertices.
+        This is a marker for the back boundary.
+
+        Returns
+        -------
+        float
+        """
         return np.max([v.y for v in self.vertices])
 
     @property
     def z_min(self) -> float:
+        """
+        Get the minimum z-coordinate over all vertices.
+        This is a marker for the bottom boundary.
+
+        Returns
+        -------
+        float
+        """
         return np.min([v.z for v in self.vertices])
 
     @property
     def z_max(self) -> float:
+        """
+        Get the maximum z-coordinate over all vertices.
+        This is a marker for the top boundary.
+
+        Returns
+        -------
+        float
+        """
         return np.max([v.z for v in self.vertices])
 
     def compute_volume(self, cell: Cell) -> float:
         """
         Compute the volume of a cell.
+
+        Returns
+        -------
+        float
         """
         # ======================================== 1D volumes
         if self.dim == 1:
@@ -82,9 +167,13 @@ class Mesh:
             dr = vtr - vbl
             return dr.x * dr.y
 
-    def compute_area(self, face: Face) -> List[float]:
+    def compute_area(self, face: Face) -> float:
         """
         Compute the area of a cell face.
+
+        Returns
+        -------
+        float
         """
         # ======================================== 0D faces
         if self.dim == 1:
@@ -103,9 +192,13 @@ class Mesh:
             v1 = self.vertices[face.vertex_ids[1]]
             return (v1 - v0).norm()
 
-    def compute_centroid(self, obj: Union[Cell, Face]) -> float:
+    def compute_centroid(self, obj: Union[Cell, Face]) -> Vector:
         """
         Compute the centroid of a cell.
+
+        Returns
+        -------
+        Vector
         """
         centroid = Vector()
         for vid in obj.vertex_ids:
@@ -114,7 +207,9 @@ class Mesh:
 
     def establish_connectivity(self) -> None:
         """
-        Establish the neighbor connectivity of the mesh.
+        Establish the neighbor connectivity of the mesh. This
+        routine is very slow and should only be used when the
+        connectivity of a mesh cannot be determined a priori.
         """
         # ======================================== Vertex-cell mapping
         vc_map = [set()] * len(self.vertices)
@@ -170,6 +265,11 @@ class Mesh:
                 self.boundary_cell_ids.append(cell.id)
 
     def plot_material_ids(self) -> None:
+        """
+        Plot the material IDs that live on each cell of the mesh.
+        This is a utility to ensure that materials are set correctly
+        in accordance to the user's wishes.
+        """
         fig: Figure = plt.figure()
 
         matids = [cell.material_id for cell in self.cells]
