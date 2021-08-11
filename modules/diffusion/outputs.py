9b9e4c358cbd9b477784581c479ae504340e7c7f
Zachary Hardy <zhardy1115@gmail.com>
2021-08-11 11:25:28 -0600

    Added precursors to output class.

--- modules/diffusion/outputs.py
+++ modules/diffusion/outputs.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from numpy import ndarray
 
 from pyPDEs.spatial_discretization import SpatialDiscretization
 
@@ -14,6 +15,7 @@ class Outputs:
         self.time: List[float] = []
         self.power: List[float] = []
         self.flux: List[List[ndarray]] = []
+        self.precursors: List[List[ndarray]] = []
 
     def store_grid(self, sd: SpatialDiscretization):
         self.grid.clear()
@@ -34,9 +36,16 @@ class Outputs:
         flux = [phi[g::n_grps] for g in range(n_grps)]
         self.flux.append(flux)
 
+        if solver.use_precursors:
+            n_dnps = solver.n_precursors
+            precursors = np.copy(solver.precursors)
+            precursors = [precursors[j::n_dnps] for j in range(n_dnps)]
+            self.precursors.append(precursors)
+
     def write_outputs(self, path: str = ".") -> None:
         if not os.path.isdir(path):
             os.makedirs(path)
+        os.system(f"rm -r {path}/*")
 
         time_path = os.path.join(path, "time.txt")
         np.savetxt(time_path, self.time, fmt="%.6g")
@@ -50,11 +59,22 @@ class Outputs:
         os.system(f"rm -r {flux_dirpath}/*")
 
         for g in range(len(self.flux[0])):
-            group_path = os.path.join(flux_dirpath, f"g{g}.txt")
-            np.savetxt(group_path, np.array(self.flux)[:, g])
+            filepath = os.path.join(flux_dirpath, f"g{g}.txt")
+            np.savetxt(filepath, np.array(self.flux)[:, g])
+
+        if len(self.precursors) > 0:
+            precursor_dirpath = os.path.join(path, "precursors")
+            if not os.path.isdir(precursor_dirpath):
+                os.makedirs(precursor_dirpath)
+            os.system(f"rm - r {precursor_dirpath}/*")
+
+            for j in range(len(self.precursors[0])):
+                filepath = os.path.join(precursor_dirpath, f"j{j}.txt")
+                np.savetxt(filepath, np.array(self.precursors)[:, j])
 
     def reset(self):
         self.grid.clear()
         self.time.clear()
         self.power.clear()
         self.flux.clear()
+        self.precursors.clear()
