2dd2157efc5846359a9f322e126fbfc5888570fe
Zachary Hardy <zhardy1115@gmail.com>
2021-08-24 20:12:15 -0600

    Updated heat conduction module.

--- modules/heat_conduction/heat_conduction_solver.py
+++ modules/heat_conduction/heat_conduction_solver.py
@@ -1,12 +1,14 @@
 import numpy as np
-from matplotlib import pyplot as plt
 from numpy.linalg import norm
+from numpy import ndarray
+
 from scipy.sparse.linalg import spsolve
 from scipy.sparse.linalg import gmres, LinearOperator
-from typing import List, Union, Callable, Tuple
-
-from numpy import ndarray
 from scipy.sparse import csr_matrix
+
+from matplotlib import pyplot as plt
+
+from typing import List, Union, Callable, Tuple
 
 from pyPDEs.mesh import Mesh
 from pyPDEs.spatial_discretization import (SpatialDiscretization,
@@ -14,10 +16,10 @@ from pyPDEs.spatial_discretization import (SpatialDiscretization,
 from pyPDEs.utilities.boundaries import (Boundary, DirichletBoundary,
                                          NeumannBoundary, RobinBoundary)
 
+SolverOutput = Tuple[bool, int, float]
 
 class HeatConductionSolver:
-    """
-    Class for solving heat conduction problems.
+    """Class for solving heat conduction problems.
     """
 
     def __init__(self) -> None:
@@ -37,18 +39,14 @@ class HeatConductionSolver:
         self.linear_tolerance: float = 1.0e-8
 
     def initialize(self) -> None:
-        """
-        Initialize the heat conduction solver.
+        """Initialize the heat conduction solver.
         """
         self.check_inputs()
         sd = self.discretization
-
-        # ======================================== Initialize system
         self.u = np.zeros(sd.n_nodes)
 
     def execute(self, verbose=False) -> None:
-        """
-        Execute the heat conduction solver.
+        """Execute the heat conduction solver.
         """
         print("\n***** Executing the steady-state "
               "heat conduction solver. *****\n")
@@ -62,31 +60,30 @@ class HeatConductionSolver:
         # Solve nonlinear problem
         else:
             converged, nit, u_change = None, None, None
+
+            # Picard iterations
             if self.nonlinear_method == "PICARD":
                 out = self.solve_picard_iterations(verbose)
                 converged, nit, u_change = out
+
+            # Newton iterations
             elif "NEWTON" in self.nonlinear_method:
                 out = self.solve_newton_iterations(verbose)
                 converged, nit, u_change = out
 
-            # ======================================== Print summary
+            # Print summary
             if converged:
                 msg = "***** Solver Converged *****"
             else:
-                msg = "!!!!! WARNING: Solver NOT Converged !!!!!"
-            msg += f"\nNonlinear Method:\t{self.nonlinear_method}"
-            msg += f"\nFinal Change:\t\t{u_change:.3e}"
-            msg += f"\n# of Iterations:\t{nit}"
-            print(msg)
+                msg = "***** WARNING: Solver NOT Converged *****"
+            header = "*" * len(msg)
+            print("\n".join(["", header, msg, header]))
+            print(f"Nonlinear Method:\t{self.nonlinear_method}")
+            print(f"Final Change:\t\t{u_change:.3e}")
+            print(f"# of Iterations:\t{nit}")
 
-        print("\n***** Done executing the steady-state "
-              "heat conduction solver. *****\n")
-
-    def solve_picard_iterations(
-            self, verbose: bool) -> Tuple[bool, int, float]:
-        """
-        Solve a non-linear heat conduction problem with Picard
-        iterations.
+    def solve_picard_iterations(self, verbose: bool) -> SolverOutput:
+        """Solve the problem with Picard iterations
 
         Parameters
         ----------
@@ -103,29 +100,34 @@ class HeatConductionSolver:
             The final difference between iterates.
         """
         u_ell = np.copy(self.u)
+
+        # Start iterating
         u_change, nit, converged = 1.0, 0, False
         for nit in range(self.nonlinear_max_iterations):
+
+            # Solve the system
             A = self.assemble_matrix()
             b = self.assemble_source()
             self.u = spsolve(A, b)
-            u_change = norm(self.u - u_ell)
 
+            # Check convergence
+            u_change = norm(self.u - u_ell)
+            u_ell[:] = self.u
+
+            # Iteration summary
             if verbose:
                 msg = f"Iteration {nit} --- "
                 msg += f"Difference: {u_change:1.4e}"
                 print(msg)
 
-            u_ell[:] = self.u
+            # Break, if converged
             if u_change < self.nonlinear_tolerance:
                 converged = True
                 break
         return converged, nit, u_change
 
-    def solve_newton_iterations(
-            self, verbose: bool) -> Tuple[bool, int, float]:
-        """
-        Solve a non-linear heat conduction problem with Newton
-        iterations.
+    def solve_newton_iterations(self, verbose: bool) -> SolverOutput:
+        """Solve the problem with Newton iterations
 
         Parameters
         ----------
@@ -141,9 +143,6 @@ class HeatConductionSolver:
         u_change : float
             The final difference between iterates.
         """
-
-
-
         class GMRESCounter(object):
             def __init__(self) -> None:
                 self.nit = 0
@@ -152,26 +151,37 @@ class HeatConductionSolver:
                 self.nit += 1
         counter = GMRESCounter()
 
+        # Start iterating
         u_change, nit, converged = 1.0, 0, False
         for nit in range(self.nonlinear_max_iterations):
-            r = self.residual(self.u)
+            r = self.residual(self.u)  # compute the residual
 
+            # Direct solve
             if "DIRECT" in self.nonlinear_method:
-                J = self.jacobian(self.u, r)
+                J = self.jacobian(self.u, r)  # define Jacobian
                 du = np.linalg.solve(J, -r)
+
+            # GMRES based solve
             else:
+                # Determine whether JFNK, or not
                 method = self.nonlinear_method
                 jfnk = False if "GMRES" in method else True
+
+                # Construct the Jacocian, or its action
                 J = self.jacobian(self.u, r, jfnk=jfnk)
+
+                # Solve using GMRES
                 counter.nit = 0
                 du = gmres(J, -r, x0=self.u, restart=1000,
                            maxiter=10*self.discretization.n_nodes,
                            tol=self.linear_tolerance,
                            callback=counter)[0]
 
+            # Check convergence
             u_change = norm(du)
             self.u += du
 
+            # Print iteration summary
             if verbose:
                 msg = f"Iteration {nit:>3} --- "
                 msg += f"Difference: {u_change:^.4e}"
@@ -179,14 +189,14 @@ class HeatConductionSolver:
                     msg += f" --- Linear Iterations: {counter.nit}"
                 print(msg)
 
+            # Break, if converged
             if u_change < self.nonlinear_tolerance:
                 converged = True
                 break
         return converged, nit, u_change
 
     def residual(self, u: ndarray) -> ndarray:
-        """
-        Compute the residual associated with a given state.
+        """Compute the residual associated with a given state.
 
         Parameters
         ----------
@@ -203,8 +213,8 @@ class HeatConductionSolver:
 
     def jacobian(self, u: ndarray, r: ndarray,
                  jfnk=False) -> Union[ndarray, LinearOperator]:
-        """
-        Compute the Jacobian matrix associated with a given state.
+        """Compute the Jacobian matrix associated with a given state.
+
         This can be computed as an actual matrix by perturbing
         the vector `u` element-wise or by defining the action
         of the Jacobian on a vector.
@@ -228,13 +238,18 @@ class HeatConductionSolver:
         eps_m = np.finfo(float).eps
         n_nodes = self.discretization.n_nodes
 
+        # Construct the numerical Jacobian
         if not jfnk:
             J = np.zeros((n_nodes,) * 2)
+
+            # Perturb each DoF, compute sensitivity
             for idof in range(n_nodes):
                 eps = np.zeros(n_nodes)
                 eps[idof] = (1.0 + np.abs(u[idof])) * np.sqrt(eps_m)
                 rp = self.residual(u + eps)
                 J[:, idof] = (rp - r) / eps[idof]
+
+        # Construct the Jacobian action function
         else:
             def jacobian_action(v):
                 if np.all(v == 0):
@@ -248,8 +263,7 @@ class HeatConductionSolver:
         return J
 
     def assemble_matrix(self) -> csr_matrix:
-        """
-        Assemble the heat conduction matrix.
+        """Assemble the heat conduction matrix.
 
         Returns
         -------
@@ -257,10 +271,12 @@ class HeatConductionSolver:
         """
         pwc: PiecewiseContinuous = self.discretization
 
-        # ======================================== Loop over cells
+        # Loop over cells
         rows, cols, data = [], [], []
         for cell in self.mesh.cells:
             view = pwc.fe_views[cell.id]
+
+            # Evaluate the conductivity
             k = self.k[cell.material_id]
             if callable(k):
                 u = self.u[view.node_ids]
@@ -269,16 +285,16 @@ class HeatConductionSolver:
             else:
                 k = np.array([k] * view.n_qpoints)
 
-            # =================================== Loop over quadrature
+            # Loop over quadrature
             for qp in range(view.n_qpoints):
                 jxw = view.jxw[qp]
 
-                # ============================== Loop over test functions
+                # Loop over test functions
                 for i in range(view.n_nodes):
                     ii = pwc.map_dof(cell, i)
                     grad_i = view.grad_shape_values[i][qp]
 
-                    # ========================= Loop over trial functions
+                    # Loop over trial functions
                     for j in range(view.n_nodes):
                         jj = pwc.map_dof(cell, j)
                         grad_j = view.grad_shape_values[j][qp]
@@ -288,16 +304,16 @@ class HeatConductionSolver:
                         cols.append(jj)
                         data.append(value)
 
-            # ======================================== Loop over faces
+            # Loop over faces
             for f_id, face in enumerate(cell.faces):
                 if not face.has_neighbor:
                     bndry_id = -1 * (face.neighbor_id + 1)
                     bc = self.boundaries[bndry_id]
 
-                    # ============================== Dirichlet boundary
+                    # Dirichlet boundary
                     if issubclass(type(bc), DirichletBoundary):
 
-                        # ==================== Loop over face nodes
+                        # Loop over face nodes
                         n_face_nodes = len(view.face_node_mapping[f_id])
                         for fi in range(n_face_nodes):
                             ii = pwc.map_face_dof(cell, f_id, fi)
@@ -306,31 +322,32 @@ class HeatConductionSolver:
                             cols.append(ii)
                             data.append(1.0)
 
-                    # ============================== Robin boundary
+                    # Robin boundary
                     elif issubclass(type(bc), RobinBoundary):
                         bc: RobinBoundary = bc
 
-                        # ==================== Loop over face nodes
+                        # Loop over face nodes
                         n_face_nodes = len(view.face_node_mapping[f_id])
                         for fi in range(n_face_nodes):
                             ni = view.face_node_mapping[f_id][fi]
                             ii = pwc.map_face_dof(cell, f_id, fi)
 
-                            # =============== Loop over face nodes
+                            # Loop over face nodes
                             for fj in range(n_face_nodes):
                                 nj = view.face_node_mapping[f_id][fj]
                                 jj = pwc.map_face_dof(cell, f_id, fj)
 
-                                value = bc.a / bc.b
-                                value *= view.intS_shapeI_shapeJ[f_id][ni][nj]
+                                intS_mass_ij = \
+                                    view.intS_shapeI_shapeJ[f_id][ni][nj]
+
+                                value = bc.a / bc.b * intS_mass_ij
                                 rows.append(ii)
                                 cols.append(jj)
                                 data.append(value)
         return csr_matrix((data, (rows, cols)), shape=(pwc.n_nodes,) * 2)
 
     def assemble_source(self) -> ndarray:
-        """
-        Assemble the right-hand side source vector.
+        """Assemble the right-hand side.
 
         Returns
         -------
@@ -338,52 +355,52 @@ class HeatConductionSolver:
         """
         pwc: PiecewiseContinuous = self.discretization
 
-        # ======================================== Loop over cells
+        # Loop over cells
         b = np.zeros(pwc.n_nodes)
         for cell in self.mesh.cells:
             view = pwc.fe_views[cell.id]
             q = self.q[cell.material_id]
 
-            # =================================== Loop test functions
+            # Loop test functions
             for i in range(view.n_nodes):
                 ii = pwc.map_dof(cell, i)
 
-                # ============================== Loop over quadrature
+                # Loop over quadrature
                 for qp in range(view.n_qpoints):
                     b[ii] += q * view.shape_values[i][qp] * view.jxw[qp]
 
-            # =================================== Loop over faces
+            # Loop over faces
             for f_id, face in enumerate(cell.faces):
                 if not face.has_neighbor:
                     bndry_id = -1 * (face.neighbor_id + 1)
                     bc = self.boundaries[bndry_id]
 
-                    # ============================== Dirichlet boundary
+                    # Dirichlet boundary
                     if issubclass(type(bc), DirichletBoundary):
                         bc: DirichletBoundary = bc
 
-                        # ==================== Loop over face nodes
+                        # Loop over face nodes
                         n_face_nodes = len(view.face_node_mapping[f_id])
                         for fi in range(n_face_nodes):
                             ii = pwc.map_face_dof(cell, f_id, fi)
                             b[ii] = bc.value
 
-                    # ============================== Neumann boundary
+                    # Neumann boundary
                     elif issubclass(type(bc), NeumannBoundary):
                         bc: NeumannBoundary = bc
 
-                        # ==================== Loop over face nodes
+                        # Loop over face nodes
                         n_face_nodes = len(view.face_node_mapping[f_id])
                         for fi in range(n_face_nodes):
                             ni = view.face_node_mapping[f_id][fi]
                             ii = pwc.map_face_dof(cell, f_id, fi)
                             b[ii] += bc.value * view.intS_shapeI[f_id][ni]
 
-                    # ============================== Robin boundary
+                    # Robin boundary
                     elif issubclass(type(bc), RobinBoundary):
                         bc: RobinBoundary = bc
 
-                        # ==================== Loop over face nodes
+                        # Loop over face nodes
                         n_face_nodes = len(view.face_node_mapping[f_id])
                         for fi in range(n_face_nodes):
                             ni = view.face_node_mapping[f_id][fi]
@@ -392,8 +409,7 @@ class HeatConductionSolver:
         return b
 
     def plot_solution(self, title: str = None) -> None:
-        """
-        Plot the currently stored solution.
+        """Plot the currently stored solution.
 
         Parameters
         ----------
@@ -411,8 +427,7 @@ class HeatConductionSolver:
         plt.tight_layout()
 
     def check_inputs(self) -> None:
-        """
-        Check the inputs of the solver.
+        """Check the inputs of the solver.
         """
         self._check_mesh()
         self._check_discretization()
