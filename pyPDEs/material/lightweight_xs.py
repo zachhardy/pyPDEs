e9512392ba9e0912fa5de3be36b3373b5193dcb8
Zachary Hardy <zhardy1115@gmail.com>
2021-10-01 10:46:23 -0600

    Copy xs values to keep independent.

--- pyPDEs/material/lightweight_xs.py
+++ pyPDEs/material/lightweight_xs.py
@@ -8,9 +8,9 @@ class LightWeightCrossSections:
 
     def __init__(self, xs: CrossSections) -> None:
         self._xs = xs
-        self.sigma_t = xs.sigma_t
-        self.D = xs.D
-        self.B_sq = xs.B_sq
+        self.sigma_t = np.copy(xs.sigma_t)
+        self.D = np.copy(xs.D)
+        self.B_sq = np.copy(xs.B_sq)
 
     def update(self, x: List[float]) -> None:
         if self._xs.sigma_a_function is not None:
