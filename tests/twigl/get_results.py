a354690698ccf1485dd42c339121f5069986c68b
Zachary Hardy <zhardy1115@gmail.com>
2021-11-15 12:13:07 -0700

    Added POD to TWIGL test.

--- tests/twigl/get_results.py
+++ tests/twigl/get_results.py
@@ -32,16 +32,23 @@ else:
 sim = SimulationReader(path)
 sim.read_simulation_data()
 
-sim.plot_flux_moments(0, 0, [0.0, 0.2, 0.5])
-sim.plot_power()
-plt.show()
+# sim.plot_flux_moments(0, 0, [0.0, 0.2, 0.5])
+# sim.plot_power()
+# plt.show()
 
 from rom.dmd import DMD
+from rom.pod import POD
 X = sim.create_simulation_matrix().T
-dmd = DMD(svd_rank=10, opt=False)
-dmd.fit(X)
+t = sim.times
 
-dmd.plot_scree()
-dmd.plot_rankwise_errors()
-dmd.plot_timestep_errors()
+dmd = DMD(svd_rank=10)
+dmd.fit(X)
+# dmd.plot_scree()
+# dmd.plot_rankwise_errors()
+
+pod = POD(svd_rank=10)
+pod.fit(X, t)
+pod.plot_scree()
+pod.plot_rankwise_errors()
+
 plt.show()
