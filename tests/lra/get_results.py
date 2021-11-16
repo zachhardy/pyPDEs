2a3f0eddaf9eb8f7b9eca4e6e63f6341a50872b9
Zachary Hardy <zhardy1115@gmail.com>
2021-11-16 13:18:58 -0700

    Updated get_results from tests.

--- tests/lra/get_results.py
+++ tests/lra/get_results.py
@@ -15,28 +15,7 @@ path = os.path.join(script_path, 'outputs')
 sim = SimulationReader(path)
 sim.read_simulation_data()
 
-# sim.plot_power(mode=1, log_scale=True)
-# sim.plot_temperatures(mode=0)
-
-from pydmd.dmd import DMD
-from pydmd.mrdmd import MrDMD
-from rom.pod import POD
-X = sim.create_simulation_matrix(variables='power_density').T
-
-pod = POD(svd_rank=1.0-1.0e-12)
-pod.fit(X, sim.times, verbose=True)
-pod.plot_rankwise_errors()
-
+sim.plot_flux_moments(0, 0, [0.0, 1.44])
+sim.plot_power(mode=1, log_scale=True)
+sim.plot_temperatures(mode=0)
 plt.show()
-
-# dmd = DMD(svd_rank=10, opt=True)
-# dmd.fit(X[:137], sim.times[:137])
-#
-# dmd1 = DMD(svd_rank=13)
-# dmd1.fit(X[137:151], sim.times[137:151])
-#
-# dmd2 = DMD(svd_rank=40)
-# dmd2.fit(X[151:], sim.times[151:])
-#
-# plt.show()
-
--- tests/twigl/get_results.py
+++ tests/twigl/get_results.py
@@ -32,23 +32,6 @@ else:
 sim = SimulationReader(path)
 sim.read_simulation_data()
 
-# sim.plot_flux_moments(0, 0, [0.0, 0.2, 0.5])
-# sim.plot_power()
-# plt.show()
-
-from rom.dmd import DMD
-from rom.pod import POD
-X = sim.create_simulation_matrix().T
-t = sim.times
-
-dmd = DMD(svd_rank=10)
-dmd.fit(X)
-# dmd.plot_scree()
-# dmd.plot_rankwise_errors()
-
-pod = POD(svd_rank=10)
-pod.fit(X, t)
-pod.plot_scree()
-pod.plot_rankwise_errors()
-
+sim.plot_flux_moments(0, 0, [0.0, 0.2, 0.5])
+sim.plot_power()
 plt.show()
