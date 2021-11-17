d443866a9f781ab2861595e288809a6e25da4ed2
Zachary Hardy <zhardy1115@gmail.com>
2021-11-16 20:43:57 -0700

    Added angular operators, harmonics, etc.

--- modules/neutron_transport/steadystate_solver/__init__.py
+++ modules/neutron_transport/steadystate_solver/__init__.py
@@ -8,6 +8,8 @@ from pyPDEs.utilities import UnknownManager
 from pyPDEs.utilities.boundaries import Boundary
 from pyPDEs.utilities.quadratures import ProductQuadrature
 
+from ._harmonics import HarmonicIndex
+
 
 class SteadyStateSolver:
     """
@@ -19,10 +21,13 @@ class SteadyStateSolver:
                                 _check_materials,
                                 _check_boundaries)
 
+    from ._harmonics import create_harmonic_indices
+
     def __init__(self) -> None:
         self.n_groups: int = 0
-        self.n_moments: int = 0
         self.n_precursors: int = 0
+        self.n_moments: int = 0
+        self.n_angles: int = 0
         self.max_precursors: int = 0
 
         self.scattering_order: int = 0
@@ -42,8 +47,9 @@ class SteadyStateSolver:
         self.matid_to_src_map: List[int] = []
         self.cellwise_xs: List[LightWeightCrossSections] = []
 
-        # Quadrature object
+        # Quadrature information
         self.quadrature: ProductQuadrature = None
+        self.harmonic_index_map: List[HarmonicIndex] = []
 
         # Flux moment information
         self.phi: ndarray = None
@@ -52,16 +58,49 @@ class SteadyStateSolver:
 
         # Angular flux information
         self.psi: ndarray = None
-        sel.psi_uk_man: UnknownManager = None
+        self.psi_uk_man: UnknownManager = None
 
         # Precursor information
         self.precursors: ndarray = None
+
+        # Angular operators
+        self.M: ndarray = None
+        self.D: ndarray = None
 
     def initialize(self) -> None:
         """
         Initialize the solver.
         """
         self._check_inputs()
+        sd = self.discretization
+
+        # Define the harmonic index map
+        self.create_harmonic_indices()
+
+        # Define number of moments and angles
+        self.n_angles = len(self.quadrature.abscissae)
+        self.n_moments = len(self.harmonic_index_map)
+
+        # Initialize flux moments
+        self.phi_uk_man = UnknownManager()
+        for m in range(self.n_moments):
+            self.phi_uk_man.add_unknown(self.n_groups)
+        self.phi = np.zeros(sd.n_dofs(self.phi_uk_man))
+        self.phi_prev = np.zeros(sd.n_dofs(self.phi_uk_man))
+
+        # Initialize angular flux
+        self.psi_uk_man = UnknownManager()
+        for n in range(self.n_angles):
+            self.psi_uk_man.add_unknown(self.n_groups)
+        self.psi = np.zeros(sd.n_dofs(self.psi_uk_man))
+
+        # Initialize precursors
+        if self.use_precursors:
+            n_dofs = self.n_precursors * self.mesh.n_cells
+            self.precursors = np.zeros(n_dofs)
+
+        # Initialize harmonics
+        self.create_harmonic_indices()
 
     def _check_inputs(self) -> None:
         """
---
+++ modules/neutron_transport/steadystate_solver/_harmonics.py
@@ -0,0 +1,168 @@
+import numpy as np
+from numpy import ndarray
+from math import factorial, sqrt
+
+from pyPDEs.utilities.quadratures import Angle
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from . import SteadyStateSolver
+
+
+def legendre(n: int, x: float) -> float:
+    """
+    Evaluate Legendre polynomials.
+
+    Parameters
+    ----------
+    n : int
+        The Legendre polynomial order.
+    x : float
+        The point to evaluate the Legendre polynomial.
+
+    Returns
+    -------
+    float
+    """
+    if n == 0:
+        return 1.0
+    elif n == 1:
+        return x
+    else:
+        n = n - 1
+        Pn = legendre(n, x)
+        Pnm1 = legendre(n - 1, x)
+        Pnp1 = (2.0*n + 1.0)*x * Pn - n * Pnm1
+        return Pnp1 / (n + 1.0)
+
+
+def associated_legendre(ell: int, m: int, x: float) -> float:
+    """
+    Evaluate associated Legendre polynomials.
+
+    Parameters
+    ----------
+    ell : int
+    m : int
+    x : float
+
+    Returns
+    -------
+    float
+    """
+    if abs(m) > ell:
+        return 0.0
+    if ell == 0:
+        return 1.0
+    elif ell == 1 and m == 0:
+        return x
+    elif ell == m:
+        ell = ell - 1
+        c = -(2.0*ell + 1.0) * np.sqrt(1.0 - x**2)
+        return c * associated_legendre(ell, ell, x)
+    else:
+        ell = ell - 1
+        Plm = associated_legendre(ell, m, x)
+        Plm1m = associated_legendre(ell-1, m, x)
+        Plp1m = (2.0*ell + 1.0)*x * Plm - (ell + m) * Plm1m
+        return Plp1m / (ell - m + 1.0)
+
+
+def spherical_harmonics(ell: int, m: int,
+                        varphi: float, theta: float) -> float:
+    """
+    Evaluate spherical harmonic functions.
+
+    Parameters
+    ----------
+    ell : int
+    m : int
+    varphi : float
+    theta : float
+
+    Returns
+    -------
+    float
+    """
+    Plm = associated_legendre(ell, m, np.cos(theta))
+
+    if m == 0:
+        return Plm
+    else:
+        fm = factorial(ell - abs(m))
+        fp = factorial(ell + abs(m))
+        c = (-1.0)**(abs(m)) * sqrt(2.0 * fm/fp) * Plm
+        if m < 0:
+            return c * np.sin(abs(m) * varphi)
+        else:
+            return c * np.cos(abs(m) * varphi)
+
+
+class HarmonicIndex:
+    """
+    Structure for spherical harmonic indices.
+    """
+    def __init__(self, ell: int, m: int) -> None:
+        self.ell: int = ell
+        self.m: int = m
+
+    def __eq__(self, other: 'HarmonicIndex') -> bool:
+        return self.ell == other.ell and self.m == other.m
+
+
+def create_harmonic_indices(self: 'SteadyStateSolver') -> None:
+    """
+    Generate the harmonic index ordering.
+    """
+    self.harmonic_index_map.clear()
+    if self.mesh.dim == 1:
+        for ell in range(self.scattering_order + 1):
+            self.harmonic_index_map.append(HarmonicIndex(ell, 0))
+    elif self.mesh.dim == 2:
+        for ell in range(self.scattering_order + 1):
+            for m in range(-ell, ell + 1, 2):
+                if ell == 0 or m != 0:
+                    self.harmonic_index_map(HarmonicIndex(ell, m))
+    else:
+        for ell in range(self.scattering_order + 1):
+            for m in range(-ell, ell + 1):
+                self.harmonic_index_map(HarmonicIndex(ell, m))
+
+
+def discrete_to_moment_matrix(self: 'SteadyStateSolver') -> None:
+    """
+    Build the moment to discrete operator.
+    """
+    self.D = np.zeros((self.n_moments, self.n_angles))
+
+    # Loop over moments
+    for m in range(self.n_moments):
+        idx = self.harmonic_index_map[m]
+
+        # Loop over angles
+        for n in range(self.n_angles):
+            w = self.quadrature.weights[n]
+            angle = self.quadrature.abscissae[n]
+            Ylm = spherical_harmonics(idx.ell, idx.m,
+                                      angle.varphi, angle.theta)
+
+            self.D[m, n] = Ylm * w
+
+
+def moment_to_discrete_operator(self: 'SteadyStateSolver') -> None:
+    """
+    Build the discrete to moment operator.
+    """
+    self.M = np.zeros((self.n_moments, self.n_angles))
+
+    # Loop over moments
+    weight_sum = sum(self.quadrature.weights)
+    for m in range(self.n_moments):
+        idx = self.harmonic_index_map[m]
+
+        # Loop over angles
+        for n in range(self.n_angles):
+            angle = self.quadrature.abscissae[n]
+            Ylm = spherical_harmonics(idx.ell, idx.m,
+                                      angle.varphi, angle.theta)
+            self.M[m, n] = (2.0*idx.ell + 1.0)/weight_sum * Ylm
