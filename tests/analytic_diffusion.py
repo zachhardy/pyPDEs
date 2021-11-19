9192299f6ff874523bfa67184429e762414e46d6
Zachary Hardy <zhardy1115@gmail.com>
2021-11-19 09:29:39 -0700

    Update analytic diffusion test.

--- tests/analytic_diffusion.py
+++ tests/analytic_diffusion.py
@@ -11,29 +11,21 @@ from modules.neutron_diffusion.analytic import AnalyticSolution, load
 xs = CrossSections()
 xs.read_from_xs_file('xs/three_grp_us.cxs', density=0.05)
 
-rf = 6.0
+rf = 5.0
 r = symbols('r')
 ics = Matrix([1.0 - r**2/rf**2, 1.0 - r**2/rf**2, 0.0])
 
 dr = rf / 100.0
 r = np.linspace(0.5*dr, rf - 0.5*dr, 1001)
 
-exact = AnalyticSolution(xs, ics, rf, 'spherical', max_n_modes=100)
+exact = AnalyticSolution(xs, ics, rf, 'spherical', max_n_modes=1000)
 exact.execute()
-exact.plot_eigenfunction(0, 1, r)
 
+exact.modes[0].plot_eigenfunction(r)
+exact.modes[1].plot_eigenfunction(r)
 
 path = os.path.dirname(os.path.abspath(__file__))
-fpath = os.path.join(path, 'test.obj')
-exact.save(fpath)
-
-if not os.path.isfile(fpath):
-    raise FileNotFoundError(
-        f'Save method failed. {fpath} not found.')
-
-exact: AnalyticSolution = load(fpath)
-exact.plot_eigenfunction(0, 1, r)
-
-os.system(f'rm {fpath}')
+path = os.path.join(path, 'three_group_sphere', 'transient')
+exact.save(path + f'/sphere{int(rf)}cm.obj')
 
 plt.show()
