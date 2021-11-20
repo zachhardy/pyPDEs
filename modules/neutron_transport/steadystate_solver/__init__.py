31a9cf703c0aafea4557887ee3bfd969063006fd
Zachary Hardy <zhardy1115@gmail.com>
2021-11-19 19:14:30 -0700

    Updated transport module.

--- modules/neutron_transport/steadystate_solver/__init__.py
+++ modules/neutron_transport/steadystate_solver/__init__.py
@@ -4,12 +4,36 @@ from numpy import ndarray
 from pyPDEs.mesh import Mesh
 from pyPDEs.material import *
 from pyPDEs.spatial_discretization import *
-from pyPDEs.utilities import UnknownManager
+from pyPDEs.utilities import Vector, UnknownManager
 from pyPDEs.utilities.boundaries import Boundary
 from pyPDEs.utilities.quadratures import ProductQuadrature
 
 from ..boundaries import *
-from ._harmonics import HarmonicIndex
+
+
+class HarmonicIndex:
+    """
+    Structure for spherical harmonic indices.
+    """
+
+    def __init__(self, ell: int, m: int) -> None:
+        self.ell: int = ell
+        self.m: int = m
+
+    def __eq__(self, other: 'HarmonicIndex') -> bool:
+        return self.ell == other.ell and self.m == other.m
+
+
+class AngleSet:
+    """
+    Implementation for an angle set.
+
+    An angle set is defined as a collection of angles that
+    share a sweep ordering.
+    """
+    def __init__(self) -> None:
+        self.angles: List[int] = []
+        self.sweep_ordering: List[int] = []
 
 
 class SteadyStateSolver:
@@ -22,11 +46,12 @@ class SteadyStateSolver:
     from ._initialize_boundaries import initialize_bondaries
     from ._initialize_unknowns import initialize_unknowns
 
-    from ._harmonics import create_harmonic_indices
     from ._angular_operators import (discrete_to_moment_matrix,
                                      moment_to_discrete_matrix)
 
     from ._setsource import set_source
+    from ._source_iterations import source_iterations
+    from ._sweep import sweep
 
     def __init__(self) -> None:
         self.n_groups: int = 0
@@ -60,6 +85,7 @@ class SteadyStateSolver:
         self.quadrature: ProductQuadrature = None
         self.harmonic_index_map: List[HarmonicIndex] = []
         self.angle_aggregation_type: str = 'octant'
+        self.angle_sets: List[AngleSet] = []
 
         # Unknown managers
         self.phi_uk_man: UnknownManager = None
@@ -71,6 +97,7 @@ class SteadyStateSolver:
 
         # Angular flux vector
         self.psi: ndarray = None
+        self.psi_outflow: ndarray = None
 
         # Precursor vector
         self.precursors: ndarray = None
@@ -81,9 +108,6 @@ class SteadyStateSolver:
         # Angular operators
         self.M: ndarray = None
         self.D: ndarray = None
-
-        # Angle Sets
-        self.angle_sets: List[List[int]] = None
 
     def initialize(self, verbose: bool = True) -> None:
         """
@@ -142,3 +166,50 @@ class SteadyStateSolver:
         """
         self.create_harmonic_indices()
         return len(self.harmonic_index_map)
+
+    def create_harmonic_indices(self) -> None:
+        """
+        Generate the harmonic index ordering.
+        """
+        self.harmonic_index_map.clear()
+        if self.mesh.dim == 1:
+            for ell in range(self.scattering_order + 1):
+                self.harmonic_index_map.append(HarmonicIndex(ell, 0))
+        elif self.mesh.dim == 2:
+            for ell in range(self.scattering_order + 1):
+                for m in range(-ell, ell + 1, 2):
+                    if ell == 0 or m != 0:
+                        self.harmonic_index_map.append(HarmonicIndex(ell, m))
+        else:
+            for ell in range(self.scattering_order + 1):
+                for m in range(-ell, ell + 1):
+                    self.harmonic_index_map.append(HarmonicIndex(ell, m))
+
+    def create_angle_sets(self) -> None:
+        """
+        Create the angle sets.
+        """
+        # Clear current angle sets
+        self.angle_sets.clear()
+
+        # 1D problems
+        if self.mesh.dim == 1:
+            sweep_order = list(range(self.mesh.n_cells))
+
+            # Rightward directions
+            angle_set = AngleSet()
+            angle_set.sweep_ordering = sweep_order
+            for i in range(self.n_angles):
+                omega = self.quadrature.omegas[i]
+                if omega.z > 0.0:
+                    angle_set.angles.append(i)
+            self.angle_sets.append(angle_set)
+
+            # Leftward directions
+            angle_set = AngleSet()
+            angle_set.sweep_ordering = sweep_order[::-1]
+            for i in range(self.n_angles):
+                omega = self.quadrature.omegas[i]
+                if omega.z < 0.0:
+                    angle_set.angles.append(i)
+            self.angle_sets.append(angle_set)
