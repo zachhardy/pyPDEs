cd8cfbcfae81e419d117b3c028d5a40c12993237
Zachary Hardy <zhardy1115@gmail.com>
2022-01-14 12:46:30 -0700

    Unimportant change.

--- studies/three_group_sphere/get_results.py
+++ studies/three_group_sphere/get_results.py
@@ -16,8 +16,7 @@ if len(sys.argv) != 4:
         f'to the problem type, study, and simulation number.')
 
 problem = int(sys.argv[1])
-case = int(sys.argv[2])
-
+study = int(sys.argv[2])
 
 # Get problem name
 if problem == 0:
@@ -28,12 +27,12 @@ else:
     raise ValueError('Invalid problem provided..')
 
 # Get parameter study name
-if case == 0:
-    case_name = 'density'
-elif case == 1:
-    case_name = 'size'
-elif case == 2:
-    case_name = 'density_size'
+if study == 0:
+    study_name = 'density'
+elif study == 1:
+    study_name = 'size'
+elif study == 2:
+    study_name = 'density_size'
 else:
     raise ValueError('Invalid case provided.')
 
@@ -44,16 +43,24 @@ else:
     sim_name = sys.argv[3].zfill(3)
 
 # Define path
-path = f'{path}/outputs/{problem_name}/{case_name}/{sim_name}'
+path = f'{path}/outputs/{problem_name}/{study_name}'
 
 # Check path
-if not os.path.isdir(path):
+data_path = f'{path}/{sim_name}'
+if not os.path.isdir(data_path):
     raise NotADirectoryError('Invalid path.')
 
 ########################################
 # Parse the results
 ########################################
-sim = NeutronicsSimulationReader(path)
+params_path = f'{path}/params.txt'
+params = np.loadtxt(params_path)
+if params.ndim == 1:
+    params = params.reshape(-1, 1)
+
+param_names = data_path.split('/')[-1].split('_')
+
+sim = NeutronicsSimulationReader(data_path)
 sim.read_simulation_data()
 
 sim.plot_flux_moments(0, [0, 1, 2], [0.0, 0.01])
