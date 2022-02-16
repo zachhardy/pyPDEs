f48f2f3f9b63b5d836151a1e08baa0e7929d925c
Zachary Hardy <zhardy1115@gmail.com>
2022-02-16 14:10:37 -0700

    Updated twigl tests.

--- tests/twigl/get_results.py
+++ tests/twigl/get_results.py
@@ -5,25 +5,21 @@ import matplotlib.pyplot as plt
 
 from readers import NeutronicsSimulationReader
 
-try:
-    if len(sys.argv) != 2:
-        raise AssertionError(
-            'There must be a command line argument to point to '
-            'the test case.\n'
-            'Options are:\n '
-            '\t0 = Ramp Reactivity Increase\n'
-            '\t1 = Step Reactivity Increase')
+if len(sys.argv) != 2:
+    raise AssertionError(
+        'There must be a command line argument to point to '
+        'the test case.\n'
+        'Options are:\n '
+        '\t0 = Ramp Reactivity Increase\n'
+        '\t1 = Step Reactivity Increase')
 
-    arg = int(sys.argv[1])
-    if arg > 1:
-        raise ValueError('Unrecognized result index.')
-except BaseException as err:
-    print(); print(err.args[0]); print()
-    sys.exit()
+arg = int(sys.argv[1])
+if arg > 1:
+    raise ValueError('Unrecognized result index.')
 
 
-script_path = os.path.dirname(os.path.abspath(__file__))
-base = os.path.join(script_path, 'outputs')
+path = os.path.dirname(os.path.abspath(__file__))
+base = os.path.join(path, 'outputs')
 if arg == 0:
     path = os.path.join(base, 'ramp')
 else:
@@ -32,6 +28,10 @@ else:
 sim = NeutronicsSimulationReader(path)
 sim.read_simulation_data()
 
-sim.plot_flux_moments(0, 0, [0.5], grouping='time')
+sim.plot_flux_moments(0, [0, 1], [0.0, 0.5], grouping='group')
+plt.gcf().suptitle("")
+for g, ax in enumerate(plt.gcf().get_axes()[::2]):
+    ax.set_title(f"Group {g}", fontsize=12)
+
 sim.plot_power()
 plt.show()
--- tests/twigl/xs.py
+++ tests/twigl/xs.py
@@ -26,8 +26,6 @@ def sigma_a_step(g: int, x: List[float], sigma_a: float) -> float:
         return sigma_a
 
 
-trnsfr = [[0.0, 0.01], [0.0, 0.0]]
-
 nu = 2.43
 beta = 0.0075
 nu_prompt = [(1.0 - beta) * nu] * 2
@@ -44,7 +42,7 @@ xs_material_0 = \
        {'n_groups': 2, 'n_precursors': 1,
         'D': [1.4, 0.4], 'sigma_a': [0.01, 0.15],
         'sigma_f': [0.007/nu, 0.2/nu],
-        'transfer_matrix': trnsfr,
+        'transfer_matrix': [[0.0, 0.01], [0.0, 0.0]],
         'nu_prompt': nu_prompt, 'nu_delayed': nu_delayed,
         'chi_prompt': chi_prompt, 'chi_delayed': chi_delayed,
         'precursor_lambda': decay, 'velocity': velocity}
@@ -53,7 +51,7 @@ xs_material_1 = \
        {'n_groups': 2, 'n_precursors': 1,
         'D': [1.3, 0.5], 'sigma_a': [0.008, 0.05],
         'sigma_f': [0.003/nu, 0.06/nu],
-        'transfer_matrix': trnsfr,
+        'transfer_matrix': [[0.0, 0.01], [0.0, 0.0]],
         'nu_prompt': nu_prompt, 'nu_delayed': nu_delayed,
         'chi_prompt': chi_prompt, 'chi_delayed': chi_delayed,
         'precursor_lambda': decay, 'velocity': velocity}
