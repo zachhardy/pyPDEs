11d21f77708f701a4b41829f678849e3b363a4dc
Zachary Hardy <zhardy1115@gmail.com>
2022-10-05 16:47:14 -0600

    refactor

--- interpolant_study.py
+++ interpolant_study.py
@@ -9,8 +9,10 @@ from numpy.linalg import norm
 
 from typing import Union
 
+from utils import get_reader
 from utils import get_dataset
 from utils import get_hyperparams
+from utils import train_test_split
 
 from readers import NeutronicsDatasetReader
 
@@ -18,12 +20,12 @@ from pyROMs.pod import POD_MCI
 
 
 def interpolant_study(
-        dataset: NeutronicsDatasetReader,
+        X_train: np.ndarray,
+        X_test: np.ndarray,
+        Y_train: np.ndarray,
+        Y_test: np.ndarray,
         pod_mci: POD_MCI,
-        variables: Union[str, list[str]] = None,
-        test_size: float = 0.2,
-        interior_only: bool = False,
-        seed: int = None
+        filename: str = None
 ) -> dict:
     """
     Perform an interpolant study to examine error as a function of
@@ -31,35 +33,19 @@ def interpolant_study(
 
     Parameters
     ----------
-    dataset : NeutronicsDatasetReader
+    X_train : numpy.ndarray
+    X_test : numpy.ndarray
+    Y_train : numpy.ndarray
+    Y_test : numpy.ndarray
     pod_mci : POD_MCI
-    variables : str or list[str], default None
-        The variables from the dataset to fit the POD-MCI model to.
-    test_size : float
-        The fraction of samples to use for validation.
-    interior_only : bool, default False
-        A flag for excluding boundary samples from the test set.
-    seed : int or None, default None
-        The random number seed.
+    filename : str, default None.
+        A location to save the plot to, if specified.
+
 
     Returns
     -------
     dict
     """
-
-    ##################################################
-    # Define the train/test split
-    ##################################################
-
-    print("Defining the training and test set...")
-    splits = dataset.train_test_split(
-        variables=variables,
-        test_size=test_size,
-        interior_only=interior_only,
-        seed=seed,
-    )
-
-    X_train, X_test, Y_train, Y_test = splits
 
     ##################################################
     # Fitting the POD-MCI model
@@ -125,26 +111,70 @@ if __name__ == "__main__":
 
     problem_name = sys.argv[1]
     study_num = int(sys.argv[2])
-    variable_names = "power_density"
-    args = [0.2, False, None]
+
+    # Splitting parameters
+    splitting_method = "random"
+    test_size = 0.2
+    interior = False,
+    seed = None
+
+    # Which particular problem
+    case = 0
+
+    # Save outputs?
+    save = False
 
     # Parse the command line
     if len(sys.argv) > 3:
         for arg in sys.argv[3:]:
             argval = arg.split("=")[1]
-            if "test_size=" in arg:
-                args[0] = float(argval)
+            if "split=" in arg:
+                splitting_method = argval
+            elif "test_size=" in arg:
+                test_size = float(argval)
             elif "interior=" in arg:
-                args[1] = bool(int(argval))
+                interior = bool(int(argval))
             elif "seed=" in arg:
-                args[2] = int(argval)
+                seed = int(argval)
+            elif "case=" in arg:
+                case = int(argval)
+            elif "save=" in arg:
+                save = bool(int(argval))
 
-    # Get the dataset
-    data = get_dataset(problem_name, study_num)
+    ##################################################
+    # Get the data
+    ##################################################
 
-    # Initialize the ROM
+    print("Getting the data...")
+
+    reader = get_reader(problem_name, study_num)
+    X, Y = get_dataset(reader, problem_name, case)
     hyperparams = get_hyperparams(problem_name)
-    rom = POD_MCI(**hyperparams)
 
-    # Perform the interpolant study
-    interpolant_study(data, rom, variable_names, *args)
+    ##################################################
+    # Split the data
+    ##################################################
+
+    if splitting_method == "random":
+        mask = reader.interior_mask if interior else None
+        splits = train_test_split(X, Y, test_size, mask, seed)
+    elif splitting_method == "uniform":
+        splits = (X[::2], X[1::2], Y[::2], Y[1::2])
+    else:
+        msg = f"{splitting_method} is not a valid splitting method."
+        raise ValueError(msg)
+
+    ##################################################
+    # Perform truncation study
+    ##################################################
+
+    # Last second check
+    interp_method = hyperparams["interpolant"]
+    if not interior:
+        if "rbf" not in interp_method and interp_method != "neighbor":
+            msg = "Only RBF and nearest neighbor interpolants are " \
+                  "permissible when extrapolation may be performed."
+            raise ValueError(msg)
+
+    rom = POD_MCI(**hyperparams)
+    interpolant_study(*splits, rom)
